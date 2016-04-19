98edd8fb386551574a6c0dec917af4b626995674
AndVad <anva6592@colorado.edu>
2016-04-19 17:59:17 -0600

    Created difficulty choices

--- main.cpp
+++ main.cpp
@@ -4,6 +4,35 @@
 using namespace std;
 
 int main(){
+    int openingMenu = 0;
+    BSTMaze dungeon;
+    bool difficultyLevel = false;
+    cout<<"========Welcome========"<<endl;
+    while(!difficultyLevel){
+        cout<<"Choose your difficulty:"<<endl;
+        cout<<"1. Easy"<<endl;
+        cout<<"2. Medium"<<endl;
+        cout<<"3. Hard"<<endl;
+        cin>>openingMenu;
+
+        if(openingMenu == 1){
+            dungeon.BSTMazeCreate(8);
+            difficultyLevel = true;
+        }
+
+        else if(openingMenu == 2){
+            dungeon.BSTMazeCreate(16);
+            difficultyLevel = true;
+        }
+
+        else if(openingMenu == 3){
+            dungeon.BSTMazeCreate(32);
+            difficultyLevel = true;
+        }
+
+        else{
+            cout<<"That was not an option."<<endl;
+        }
 
 
     return 0;
