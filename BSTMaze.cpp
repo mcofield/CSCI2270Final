7f56f5561a4ad38450b79b11afaf132b28f5ccc4
AndVad <anva6592@colorado.edu>
2016-04-19 18:02:07 -0600

    Wrote constructor, destructor

--- BSTMaze.cpp
+++ BSTMaze.cpp
@@ -3,3 +3,37 @@
 #include <iostream>
 
 using namespace std;
+
+void BSTMaze(){}
+
+void BSTMaze::BSTMazeCreate(int rooms){
+    root = NULL;
+    mazeRoom* tmp;
+    for(int x = 0; x < rooms; x++){
+        mazeRoom* n = new mazeRoom;
+        if(root == NULL){
+            root = n;
+        }
+        else{
+            tmp = root;
+            while(tmp->right != NULL){
+                tmp = tmp->right
+            }
+            tmp->right = n;
+        }
+    }
+}
+
+void BSTMaze::~BSTMaze(){
+    BSTdelete();
+}
+
+void BSTMaze::BSTDelete(mazeRoom* current){
+    if(current->right != NULL){
+        BSTDelete(current->right);
+    }
+    if(current->left != NULL){
+        BSTDelete(current->left);
+    }
+    delete current;
+}
